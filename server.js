44de58b12c6996b7d4d118366a0b16d225b13007
Steven Cable <steven.cable12@gmail.com>
2016-12-28 22:58:25 -1000

    added html sources and split string up

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 /*jshint esversion: 6*/
 
 var net = require('net');
+var htmls = require('./htmlSource.js');
+
 var helloWorld = 'hello World';
 
 var timeStamp = new Date().toUTCString();
@@ -8,12 +10,23 @@ var timeStamp = new Date().toUTCString();
 const server = net.createServer((socket) => {
 
   socket.setEncoding('utf8');
-  
+
   socket.on('data', function(data){
     process.stdout.write(`what is: ${data}`);
     var initialSplit = data.split('\r\n');
     console.log(initialSplit);
-  
+    var splitArray = [];
+
+    for(var i = 0; i<initialSplit.length; i++){
+      splitArray.push(initialSplit[i].split(' '));
+    }
+    console.log(splitArray[0][0]);
+
+    if(splitArray[0][0] === "GET"){
+      console.log('whoohoo braddah');
+    }else{
+      console.log('awww fuck');
+    }
   });
 
   socket.on('end', () => {
